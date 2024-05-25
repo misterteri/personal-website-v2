c7ab019ccb9cabf4cc81be860c2bd8441e4bb986
Marcelino Gilbert Tagore <marcelinogilbert26@gmail.com>
2024-05-25 11:52:36 +0800

    feat: viewport sizing

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -15,7 +15,7 @@ import ScrollArrow from "./ScrollArrow";
 export default function HeroSection() {
   return (
     <Grid
-      height="100vh"
+      paddingTop={"10vh"}
       templateAreas={`"name name"
                         "surname title"
                         "surname buttons"`}
@@ -31,10 +31,7 @@ export default function HeroSection() {
         justifyContent="center"
         border="1px solid black"
       >
-        <Heading
-          fontSize={["6.375vw", "10.2vw", "13.225vw", "16.5vw"]}
-          transform="scaleY(1.3)"
-        >
+        <Heading fontSize="16.5vw" transform="scaleY(1.3)">
           MARCELINO
         </Heading>
       </GridItem>
@@ -47,7 +44,7 @@ export default function HeroSection() {
         border="1px solid black"
       >
         <Heading
-          fontSize={["6.375vw", "10.2vw", "13.225vw", "18vw"]}
+          fontSize={["18vw", "18vw", "18vw", "18vw"]}
           transform="scaleY(1.3)"
         >
           GILBERT
@@ -59,48 +56,30 @@ export default function HeroSection() {
         display="flex"
         flexDirection="column"
         alignItems="left"
-        justifyContent="flex-start"
-        // make the vertical spaces between the boxes to be 1px
-        paddingTop={12}
+        padding="1vw"
       >
         <Box border="1px solid black">
-          <Text fontSize="50px" color="black">
+          <Text fontSize="3vw" color="black" fontWeight={"bold"}>
             Hsinchu, Taiwan
           </Text>
         </Box>
-        <Box
-          display="flex"
-          alignItems="center"
-          border="1px solid black"
-          padding="0px"
-        >
-          <Text fontSize="60px" fontWeight="extra-bold" color="black">
-            •
-          </Text>
-          <Text fontSize="50px" color="black">
-            Full Stack Dev.
+        <Box display="flex" border="1px solid black">
+          <Text fontSize="3vw" color="black">
+            jr. fullstack
           </Text>
         </Box>
-        <Box display="flex" alignItems="center">
-          <Text
-            fontSize="60px"
-            fontWeight="extra-bold"
-            color="black"
-            marginRight="10px"
-          >
-            •
-          </Text>
-          <Text fontSize="50px" color="black">
-            Software Eng.
+        <Box display="flex" alignItems="center" border="1px solid black">
+          <Text fontSize="3vw" color="black">
+            jr. data engineer
           </Text>
         </Box>
       </GridItem>
 
       <GridItem
         area="buttons"
+        display="flex"
+        flexDirection="column"
         alignItems="center"
-        justifyContent="space-between"
-        paddingX="10"
       >
         <Link href="resume_view">
           <Button
@@ -108,9 +87,11 @@ export default function HeroSection() {
             _hover={{ bg: "gray" }}
             color="white"
             variant="solid"
-            margin="inherit"
-            borderRadius="3px"
-            fontSize="32px"
+            paddingInline={"1vw"}
+            paddingBlockStart={"1.5vw"} // Modify padding top
+            paddingBlockEnd={"2vw"} // Modify padding bottom
+            borderRadius="5px"
+            fontSize="3vw"
           >
             view resume
           </Button>
