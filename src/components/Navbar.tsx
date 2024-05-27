16b1bb6403910c9cff007b63220330ac57146d7f
Marcelino Gilbert Tagore <marcelinogilbert26@gmail.com>
2024-05-27 11:02:07 +0800

    feat: add navbar transition

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Box, HStack, Link, Spacer, Text, Flex } from "@chakra-ui/react";
-
+import { Link as ScrollLink } from "react-scroll";
 export default function Navbar(): JSX.Element {
   return (
     <Flex
@@ -19,57 +19,70 @@ export default function Navbar(): JSX.Element {
         margin="auto"
         padding="2"
       >
-        <Link href="#herosection">
-          <Text fontSize="3xl" fontWeight="bold">
+        <ScrollLink
+          to="herosection"
+          smooth={true} // Add smooth scrolling behavior
+          duration={500} // Set the duration of the scroll animation
+          offset={-70} // Adjust the scroll offset if needed
+        >
+          <Text fontSize="3xl" fontWeight="bold" cursor="pointer">
             /dev/gilbert
           </Text>
-        </Link>
+        </ScrollLink>
       </Box>
       <Spacer />
       <HStack spacing={4}>
-        <Link
-          href="#about-me"
-          // border="1px solid #e2e8f0"
-          borderRadius="md"
-          paddingInline={1}
-          paddingBlockStart={1} // Modify padding top
-          paddingBlockEnd={2} // Modify padding bottom
-          _hover={{
-            bg: "gray.100",
-            time: "0.3s",
-          }}
-        >
-          <Text fontSize="xl">About Me</Text>
-        </Link>
+        <ScrollLink to="about-me" smooth={true} duration={500} offset={-70}>
+          <Box
+            borderRadius="md"
+            paddingInline={1}
+            paddingBlockStart={1}
+            paddingBlockEnd={2}
+            _hover={{
+              bg: "gray.100",
+              transitionDuration: "0.3s",
+            }}
+          >
+            <Text fontSize="xl" cursor="pointer">
+              About Me
+            </Text>
+          </Box>
+        </ScrollLink>
 
+        <ScrollLink to="skills" smooth={true} duration={500} offset={-70}>
+          <Box
+            borderRadius="md"
+            paddingInline={1}
+            paddingBlockStart={1}
+            paddingBlockEnd={2}
+            _hover={{
+              bg: "gray.100",
+              transitionDuration: "0.3s",
+            }}
+          >
+            <Text fontSize="xl" cursor="pointer">
+              Skills
+            </Text>
+          </Box>
+        </ScrollLink>
+        <ScrollLink to="projects" smooth={true} duration={500} offset={-70}>
+          <Box
+            borderRadius="md"
+            paddingInline={1}
+            paddingBlockStart={1}
+            paddingBlockEnd={2}
+            _hover={{
+              bg: "gray.100",
+              transitionDuration: "0.3s",
+            }}
+          >
+            <Text fontSize="xl" cursor="pointer">
+              Projects
+            </Text>
+          </Box>
+        </ScrollLink>
         <Link
-          href="#skills" // border="1px solid #e2e8f0"
-          borderRadius="md"
-          paddingInline={1}
-          paddingBlockStart={1} // Modify padding top
-          paddingBlockEnd={2} // Modify padding bottom
-          _hover={{
-            bg: "gray.100",
-            time: "0.3s",
-          }}
-        >
-          <Text fontSize="xl">Skills</Text>
-        </Link>
-        <Link
-          href="#projects" // border="1px solid #e2e8f0"
-          borderRadius="md"
-          paddingInline={1}
-          paddingBlockStart={1} // Modify padding top
-          paddingBlockEnd={2} // Modify padding bottom
-          _hover={{
-            bg: "gray.100",
-            time: "0.3s",
-          }}
-        >
-          <Text fontSize="xl">Projects</Text>
-        </Link>
-        <Link
-          href="#contact" // border="1px solid #e2e8f0"
+          href="mailto:marcelinogilbert26@gmail.com"
           borderRadius="md"
           paddingInline={1}
           paddingBlockStart={1} // Modify padding top
